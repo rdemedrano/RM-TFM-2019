ac952d1c968daa59faf2078be43aa6f624a7b2c2
Rodrigo de Medrano <rdemedrano@ndowe.uned.es>
2019-04-17 18:36:12 +0200

    Estudio preliminar del tráfico en la STNN

--- Accidentes de tráfico - Madrid/Scripts/Fase 3/stnn_trafico.R
+++ Accidentes de tráfico - Madrid/Scripts/Fase 3/stnn_trafico.R
@@ -1,22 +1,7 @@
-# PREPARACIÓN DE DATOS PARA LA LA STNN PERO CON BARRIOS Y RANGO HORARIO. 
-# SIMULACRO DE EXPERIMENTO REAL SIN METEOROLOGÍA
+# SCRIPT EN EL QUE SE GENERA EL DATASET PARA LA STNN CON LA INTENSIDAD DE TRÁFICO
 
-
-# Cargamos paquetes y dataset
 library(data.table); library(lubridate); library(plyr); library(sp); library(tidyr)
-load("../Accidentes de tráfico - Madrid/Cleaned_data/BarriosAccidentalidad.RData")
 load("../Accidentes de tráfico - Madrid/Cleaned_data/trafico_2018.RData")
-
-car_crash <- BarriosAccidentalidad[ , .N, by = list(FECHA, `RANGO HORARIO`, BARRIO,lon, lat)]
-car_crash <- na.omit(car_crash)
-
-car_crash[,FECHA := dmy(FECHA)]
-car_crash[, N := NULL]
-car_crash <- car_crash[year(FECHA) == 2018]
-car_crash$`RANGO HORARIO` <- extract_numeric(substr(car_crash$`RANGO HORARIO`, start = 1, stop = 5))
-
-car_crash <- car_crash[, .N, by = list(FECHA, `RANGO HORARIO`, BARRIO)]
-colnames(car_crash)[4] <- "Número de accidentes"
 
 # Puntos de medida espaciales
 pmed_ubicacion <- fread("Raw_data/pmed_ubicacion_10_2018.csv")
@@ -50,26 +35,22 @@ traffic_int_med$BARRIO <- as.numeric(as.character(traffic_int_med$BARRIO))
 
 
 num_dias = 45
+# Parece ser que hay algunos barrios sin puntos de medida, pero por facilitar las cosas 
+# los ponemos igual y ya se les dará el valor de NA
 num_zonas = 131
 num_horas = num_zonas*24
 fechas_horas_y_zonas <- data.table(FECHA = sort(rep(seq(ymd('2018-01-01'), ymd('2018-02-14'), by = '1 day'), num_horas)),
                                    "RANGO HORARIO" = sort(rep(seq(0,23), num_zonas)),
-                                   BARRIO = rep(sort(unique(car_crash$BARRIO)), times = num_dias))
+                                   BARRIO = rep(sort(as.numeric(as.character(barrios$CODBAR))), times = num_dias))
 
-# Ahora se hace el join con car_crash y se da 0 a todas aquellas zonas sin accidentes. A la intensidad se le da 300
-number_crash <- join(fechas_horas_y_zonas, car_crash)
-number_crash[is.na(number_crash)] <- 0
-
+# Ahora se hace el join con la intensidad, y si es NA se le da 300 que es más o menos la media.
 traffic <- join(fechas_horas_y_zonas, traffic_int_med)
 traffic[is.na(traffic)] <- 300
 
-#  Ahora hay que general el dataset final con el string en vez de número de accidentes y eso.
-# Recuerda que primero va la variable de la serie temporal, y luego las exógenas
-crash_traffic <- join(number_crash, traffic)
-crash_traffic <- within(crash_traffic, variables <- paste(`Número de accidentes`, int_med))
-crash_traffic[, c(4,5) := NULL]
+# Si quieres normalizar - estandarizar...
+traffic$int_med <- (traffic$int_med-min(traffic$int_med))/(max(traffic$int_med) - min(traffic$int_med))
 
 # Ahora simplemente se pone en forma de matriz para la entrada en la STNN mediante dcast de data.table
-crash_ex <- dcast(crash_traffic, FECHA + `RANGO HORARIO` ~ BARRIO)
-crash_ex <- crash_ex[, c("FECHA", "RANGO HORARIO") := NULL]
-# fwrite(crash_ex, file = "Raw_data/crash_ex.csv", sep = "\t", row.names = FALSE, col.names = FALSE)
+traffic_stnn <- dcast(traffic, FECHA + `RANGO HORARIO` ~ BARRIO)
+traffic_stnn <- traffic_stnn[, c("FECHA", "RANGO HORARIO") := NULL]
+# fwrite(traffic_stnn, file = "Raw_data/traffic.csv", sep = "\t", row.names = FALSE, col.names = FALSE)
