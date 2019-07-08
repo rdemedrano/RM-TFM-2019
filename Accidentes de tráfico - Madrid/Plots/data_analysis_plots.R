64379cee10c86b50b998d02e65955d61fad4daca
Rodrigo <rdemedrano@gmail.com>
2019-07-08 20:28:28 +0200

    Representaciones datos 3

--- Accidentes de tráfico - Madrid/Plots/data_analysis_plots.R
+++ Accidentes de tráfico - Madrid/Plots/data_analysis_plots.R
@@ -1,6 +1,7 @@
 # SCRIPT EN EL QUE SE GENERAN LOS PLOTS RELATIVOS AL ANÁLISIS DE DATOS
 library(data.table); library(ggplot2); library(lubridate)
 load("../Accidentes de tráfico - Madrid/Cleaned_data/number_crash.RData")
+load("../Accidentes de tráfico - Madrid/Cleaned_data/crash_traffic.RData")
 
 
 # 1_ Serie temporal sola.
@@ -17,6 +18,9 @@ ggplot(data = acc[1:672], aes(x = FECHA, y = acc)) +
   theme(axis.text = element_text(size=12),
         text = element_text(size=14))
   
+
+
+
 
 
 
@@ -55,6 +59,101 @@ ggplot(data = acc, aes(x = month(FECHA), y = acc, group = month(FECHA))) +
                               "September", "October", "November", "December")) +
   scale_y_continuous(limits = c(0,11), breaks = c(1,2,3,4,5,6,7,8,9,10,11)) +
   labs(x = "Month", y = "Number of accidents") +
+  theme_bw() +
+  theme(axis.text = element_text(size=12),
+        text = element_text(size=14),
+        axis.text.x = element_text(angle=45, hjust=1))
+
+
+
+
+
+
+
+# 3_ Dependencia espacial por distritos
+load("../Accidentes de tráfico - Madrid/Cleaned_data/GeoAccidentalidad.RData")
+GeoAccidentalidad$FECHA <- dmy(GeoAccidentalidad$FECHA)
+acc <- GeoAccidentalidad[year(FECHA) == 2018, .N, by = list(FECHA, `RANGO HORARIO`, `DIA SEMANA`, DISTRITO, lat, lon)]
+acc <- acc[,c("N", "lat", "lon") := NULL]
+
+ggplot(acc, aes(x = DISTRITO)) + 
+  geom_histogram(stat = "count", color = "black" , fill = "lightblue") + 
+  labs(x = "District", y = "Number of accidents") +
+  theme_bw() +
+  theme(axis.text = element_text(size=10),
+        text = element_text(size=14),
+        axis.text.x = element_text(angle=60, hjust=1))
+
+
+
+
+
+
+# 4_ Dependencia espacial por barrios
+library(rgdal); library(sp); library(data.table); library(plyr)
+barrios <- rgdal::readOGR("Raw_data/BARRIOS.shp")
+barrios <- spTransform(barrios, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+
+acc <- number_crash
+acc <- acc[ ,acc := sum(`Número de accidentes`), by = list(BARRIO)]
+acc$FECHA <- NULL
+acc$`Número de accidentes` <- NULL
+acc$`RANGO HORARIO` <- NULL
+acc <- unique(acc)
+colnames(acc)[1] <- "CODBAR"
+
+# Jugamos con la idea del que el id del fortify se ordena igual que como
+# salen los barrios de forma natural
+barrios@data$CODBAR <- as.numeric(as.character(barrios@data$CODBAR))
+barrios@data <- join(barrios@data, acc)
+barrios@data$id <- seq(0,130,1)
+
+barrios_for <- fortify(barrios)
+barrios_for <- join(barrios_for, barrios@data[,10:11])
+
+ggplot() +
+  geom_polygon(data = barrios_for, aes(x = long, y = lat, group = id, fill = acc)) +
+  scale_fill_gradient2(low = "green", mid = "red", high = "black", midpoint = 125) +
+  labs(x = "Longitude", y = "Latitude", fill = "Number of accidents") +
+  theme_bw() +
+  theme(axis.text = element_text(size=12),
+        text = element_text(size=14))
+
+
+
+
+# 5_ Serie temporal del tráfico: no recuerdo como solucione lo de las
+# medidas a 300.
+acc <- crash_traffic
+acc <- acc[ ,int := mean(int_med), by = list(FECHA, `RANGO HORARIO`)]
+acc[, c(3,4,5,6,7,8,9,10,11,12) := NULL]
+acc <- unique(acc)
+acc$weekday <- weekdays(acc$FECHA)
+
+ggplot(data = acc, aes(x = `RANGO HORARIO`, y = int, group = `RANGO HORARIO`)) +
+  geom_boxplot(fill = "lightgrey") +
+  scale_x_discrete(limits = seq(0,23,1)) +
+  labs(x = "Hour", y = "Traffic intensity") +
+  theme_bw() +
+  theme(axis.text = element_text(size=12),
+        text = element_text(size=14))
+
+ggplot(data = acc, aes(x = weekday, y = int, group = weekday)) +
+  geom_boxplot(fill = "lightgrey") +
+  scale_x_discrete(limits = c("Monday", "Tuesday", "Wednesday", "Thursday",
+                              "Friday", "Saturday", "Sunday")) +
+  labs(x = "Weekday", y = "Traffic intensity") +
+  theme_bw() +
+  theme(axis.text = element_text(size=12),
+        text = element_text(size=14),
+        axis.text.x = element_text(angle=45, hjust=1))
+
+ggplot(data = acc, aes(x = month(FECHA), y = int, group = month(FECHA))) +
+  geom_boxplot(fill = "lightgrey") +
+  scale_x_discrete(limits = c("January", "February", "March", "April",
+                              "May", "June", "July", "August",
+                              "September", "October", "November", "December")) +
+  labs(x = "Month", y = "Traffic intensity") +
   theme_bw() +
   theme(axis.text = element_text(size=12),
         text = element_text(size=14),
